6fbb28c346d3eff4a7e2c07ed061de9ed57c93a2
AxelRayo992 <116772641+AXELRAYO990@users.noreply.github.com>
2025-10-23 15:18:52 -0600

    Eliminado texto 'Entrega en un máximo de 7 días' de las cards de pricing, manteniendo solo 'ENTREGA EN MÁXIMO 2-14 DÍAS'

--- src/App.tsx
+++ src/App.tsx
@@ -463,12 +463,6 @@ function App() {
                   <svg className="w-5 h-5 text-success mr-3" fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                   </svg>
-                  Entrega en un máximo de 7 días
-                </li>
-                <li className="flex items-center">
-                  <svg className="w-5 h-5 text-success mr-3" fill="currentColor" viewBox="0 0 20 20">
-                    <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                  </svg>
                   <span className="font-bold text-highlight">ENTREGA EN MÁXIMO 2-14 DÍAS</span>
                 </li>
               </ul>
@@ -522,12 +516,6 @@ function App() {
                     <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                   </svg>
                   Privacidad garantizada
-                </li>
-                <li className="flex items-center">
-                  <svg className="w-5 h-5 text-success mr-3" fill="currentColor" viewBox="0 0 20 20">
-                    <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                  </svg>
-                  Entrega en un máximo de 7 días
                 </li>
                 <li className="flex items-center">
                   <svg className="w-5 h-5 text-success mr-3" fill="currentColor" viewBox="0 0 20 20">
@@ -588,12 +576,6 @@ function App() {
                   <svg className="w-5 h-5 text-success mr-3" fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                   </svg>
-                  Entrega en un máximo de 7 días
-                </li>
-                <li className="flex items-center">
-                  <svg className="w-5 h-5 text-success mr-3" fill="currentColor" viewBox="0 0 20 20">
-                    <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                  </svg>
                   <span className="font-bold text-highlight">ENTREGA EN MÁXIMO 2-14 DÍAS</span>
                 </li>
               </ul>
@@ -644,12 +626,6 @@ function App() {
                     <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                   </svg>
                   Privacidad garantizada
-                </li>
-                <li className="flex items-center">
-                  <svg className="w-5 h-5 text-success mr-3" fill="currentColor" viewBox="0 0 20 20">
-                    <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                  </svg>
-                  Entrega en un máximo de 7 días
                 </li>
                 <li className="flex items-center">
                   <svg className="w-5 h-5 text-success mr-3" fill="currentColor" viewBox="0 0 20 20">
